728c7f67fc307141c0f042a46116f8e180070632
eeegi <david.kimmon.lee@lge.com>
2020-08-15 12:37:21 +0900

    [BST] Change BST to list by leve unit

--- EngeerInKorea/BinaryTree.java
+++ EngeerInKorea/BinaryTree.java
@@ -1,5 +1,8 @@
 package EngeerInKorea;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class BinaryTree {
 	class Node {
 		int data;
@@ -11,6 +14,24 @@ public class BinaryTree {
 	}
 	
 	Node root;
+	
+	public BinaryTree() {
+		
+	}
+	
+	public BinaryTree(int size) {
+		root = makeBST(0, size -1);
+	}
+	
+	Node makeBST (int start, int end) {
+		if (start > end) return null;
+		int mid = (start + end) / 2;
+		Node node = new Node(mid);
+		node.left = makeBST(start, mid -1);
+		node.right = makeBST(mid +1, end);
+		return node;
+	}
+	
 	public void makeTree(int[] a) {
 		root = makeTreeR(a, 0, a.length - 1);
 	}
@@ -36,6 +57,52 @@ public class BinaryTree {
 		}
 	}
 	
+	ArrayList<LinkedList<Node>> BSTtoList() {
+		ArrayList<LinkedList<Node>> lists = new ArrayList<LinkedList<Node>>();
+		BSTtoList(root, lists, 0);
+		return lists;
+	}
+	
+	void BSTtoList(Node root, ArrayList<LinkedList<Node>> lists, int level) {
+		if (root == null) return;
+		LinkedList<Node> list = null;
+		if (lists.size() == level) { // ?
+			list = new LinkedList<Node>();
+			lists.add(list);
+		} else {
+			list = lists.get(level);
+		}
+		list.add(root);
+		BSTtoList(root.left, lists, level + 1);
+		BSTtoList(root.right, lists, level + 1);
+	}
+	
+	ArrayList<LinkedList<Node>> BSTtoList2() {
+		ArrayList<LinkedList<Node>> result = new ArrayList<LinkedList<Node>>();
+		LinkedList<Node> current = new LinkedList<Node>();
+		if (root != null) {
+			current.add(root);
+		}
+		while (current.size() > 0) {
+			result.add(current);
+			LinkedList<Node> parents = current;
+			current = new LinkedList<Node>();
+			for (Node parent : parents) {
+				if (parent.left != null) current.add(parent.left);
+				if (parent.right != null) current.add(parent.right);
+			}
+		}
+		return result;
+	}
+	
+	void printList(ArrayList<LinkedList<Node>> arr) {
+		for (LinkedList<Node> list : arr)  {
+			for (Node node : list) {
+				System.out.print(node.data + " ");
+			}
+			System.out.println();
+		}
+	}
 	
 	public static void main(String[] args) {
 		/*
---
+++ EngeerInKorea/Tree_BinaryTreeByLevelUnit.java
@@ -0,0 +1,15 @@
+package EngeerInKorea;
+
+/*
+ * 이진트리의 노드들을 각 레벨 별로 LinkedList에 담는 알고리즘을 구현하시오.
+ * 예를 들어, 5개의 깊이를 가지는 트리라면 5개의  LinkedList를 만들어야함
+ * time : O(N), space O(N)
+ */
+public class Tree_BinaryTreeByLevelUnit {
+
+	public static void main(String[] args) {
+		BinaryTree t = new BinaryTree(10);
+		t.printList(t.BSTtoList());
+		t.printList(t.BSTtoList2());
+	}
+}
