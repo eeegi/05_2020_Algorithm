7735abefb9163fcf35bf45f93efaee664cf8126e
eeegi <david.kimmon.lee@lge.com>
2020-08-31 07:08:49 +0900

    [Tree] Add explanation

--- EngineerInKorea/BinaryTree.java
+++ EngineerInKorea/BinaryTree.java
@@ -3,6 +3,16 @@ package EngineerInKorea;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/*
+ *  트리는 자료가 저장된 노드(Node), 서로 연결해주는 간선(Edge)로 구성된 자료 구조이다.
+ *  두 연결된 노드 중 상위 노드를 부모(Parent), 하위 노드를 자식(Child) 노드 라고 부른다.
+ *  부모 노드와 그의 부모들을 통틀어 선조(Ancestor)라 부르고, 자식노드와 그 자식들을 통틀어 자손(Descendant)이라고 부른다.
+ *  트리에서 모든 노드들을 자손으로 갖는 노트가 딱 하나 생기는데 이 노드를 트리의 뿌리 노드 or 루트(Root)라고 부릅니다.
+ *  반대로 자식이 하나도 없는 노드들을 트리의 잎 or 리프(Leaf)라고 부릅니다.
+ *  
+ *  어떤 노드에 도달히기 위해 거쳐야 하는 간선의 수를 해당 노드의 깊이(depth)라고 하며
+ *  가장 깊숙히 있는 노드의 깊이를 해당 트리의 높이(height)라고 합니다.
+ */
 public class BinaryTree {
 	class Node {
 		int data;
@@ -14,7 +24,6 @@ public class BinaryTree {
 	}
 	
 	Node root;
-	
 	public BinaryTree() {
 		
 	}
